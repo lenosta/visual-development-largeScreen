006d91650d5dceb68c0b876d74e20c2e8f2e7789
yuyoo0508 <zhiguo0508@126.com>
2019-01-07 14:32:40 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,67 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import demo1 from 'pages/demo1'
-import demo2 from 'pages/demo2'
+import showContent from 'pages/showContent'
+import animation from 'pages/animation'
+import configuration from 'pages/configuration'
+import communication from 'pages/communication'
+import baseLayout from 'pages/baseLayout'
 Vue.use(Router)
 export default new Router({
   routes: [
     {
       path: '/', //路由默认跳转
-      redirect: 'demo1'
+      redirect: 'baseLayout'
     },
     {
-      name: 'demo1',
-      path: '/demo1',
-      component: demo1
+      name: 'baseLayout',
+      path: '/baseLayout',
+      component: baseLayout,
+      menuIcon: 'icon-huifumorenbuju',
+      isMenu: true,
+      meta: {
+        title: '基础布局'
+      }
     },
     {
-      name: 'demo2',
-      path: '/demo2',
-      component: demo2
+      name: 'showContent',
+      path: '/showContent',
+      component: showContent,
+      menuIcon: 'icon-neironggengxin',
+      isMenu: true,
+      meta: {
+        title: '内容呈现'
+      }
+    },
+    {
+      name: 'animation',
+      path: '/animation',
+      component: animation,
+      menuIcon: 'icon-jiaohuyanshidonghua',
+      isMenu: true,
+      meta: {
+        title: '动画'
+      }
+    },
+    {
+      name: 'communication',
+      path: '/communication',
+      component: communication,
+      menuIcon: 'icon-tongxin',
+      isMenu: true,
+      meta: {
+        title: '通信'
+      }
+    },
+    {
+      name: 'configuration',
+      path: '/configuration',
+      component: configuration,
+      menuIcon: 'icon-peizhi',
+      isMenu: true,
+      meta: {
+        title: '配置'
+      }
     }
-  ],
-  linkActiveClass: 'router-active'
+  ]
+  // linkActiveClass: 'router-active'
 })
